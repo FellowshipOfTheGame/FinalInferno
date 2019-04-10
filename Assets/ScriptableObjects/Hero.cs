2cea18ad06aab4419194acab873aba048c3434be
Gabriel Salgueiro <gabrielsalgueiro@hotmail.com.br>
2019-04-09 23:16:24 -0300

    Creation of Scriptable Objects

---
+++ Assets/ScriptableObjects/Hero.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SOs/Hero")]
+public class Hero : Unit{
+    public Sprite spriteOW;
+    public Sprite skillBG;
+    public Sprite portrait;
+
+}
---
+++ Assets/ScriptableObjects/PlayerSkill.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "SOs/PlayerSkill")]
+public class PlayerSkill : Skill{
+   public int level;
+   public long xp;
+   public long xpNext;
+   public string description;
+   public bool active;
+    //(...)
+}
