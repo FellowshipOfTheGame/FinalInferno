dac0f658de30344a6d8cfe901602e8b417398450
Ítalo <italo.ts4895@gmail.com>
2019-04-21 11:15:16 -0300

    Can find random encounters but missing the table of enemies per map; Prefabs folder added; SceneLoader created, but incomplete

--- Assets/Scripts/Battle/RECalculator.cs
+++ Assets/Scripts/Battle/RECalculator.cs
@@ -12,9 +12,9 @@ public class RECalculator : MonoBehaviour
     public Sprite BattleBG;
     public AudioClip BattleBGM;
     [SerializeField]
-    private float baseEncounterRate;
+    private float baseEncounterRate = 10.0f;
     [SerializeField]
-    private float rateIncreaseFactor;
+    private float rateIncreaseFactor = 0.05f;
     private float curEncounterRate;
     private Vector2 lastPosition;
 
@@ -29,21 +29,33 @@ public class RECalculator : MonoBehaviour
     void Update()
     {
         if (encountersEnabled) {
+            // Calcula a distancia entre a posicao atual e a distance no ultimo update
             float distance = Vector2.Distance(lastPosition, new Vector2(playerObj.position.x, playerObj.position.y));
             if (distance > 0) {
+                // Caso o player tenha se movido, verifica se encontrou batalha
                 CheckEncounter(distance);
+                // Atualiza lastPosition
                 lastPosition = new Vector2(playerObj.position.x, playerObj.position.y);
             }
         }
     }
 
     private void CheckEncounter(float distance) {
+        // A distancia percorrida e usada para aumentar/diminuir a chance de encontro
         if (Random.Range(0.0f, 100.0f) < curEncounterRate * (distance)) {
+            // Quando encontrar uma batalha
             Debug.Log("Found random encounter");
+            // Diminui a taxa de encontro para metade do valor base
+            // Isso reduz a chance de batalhas consecutivos
             curEncounterRate = baseEncounterRate/2;
+            // Salvar a posição atual de cada player dentro do seu respectivo SO
             // Usar a tabela de encontros aleatorios para definir a lista de inimigos
+            // To do
+            SceneLoader.LoadBattleScene(new List<Enemy>(), new int[0], BattleBG, BattleBGM);
         } else {
+            // Caso nao encontre uma batalha
             Debug.Log("Did not find random encounter");
+            // Aumenta a chance de encontro linearmente
             curEncounterRate += rateIncreaseFactor;
         }
     }
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class SceneLoader {
+    // Essa referencia precisa ser configurada quando o jogo for inicializado
+    public static Party party;
+    public static Scene lastScene;
+    public static void LoadBattleScene(List<Enemy> enemies, int[] enemyCount, Sprite BG, AudioClip BGM) {
+        lastScene = SceneManager.GetActiveScene();
+        // Iniciar animação de encounter caso tenha (Ex.: tela escurecendo)
+        // Adicionar o setup da batalha no SceneManager.sceneLoaded
+        // To do
+        SceneManager.LoadScene("Battle");
+    }
+    public static void LoadOWScene(Scene map) {
+        SceneManager.sceneLoaded += OnMapLoad;
+        SceneManager.LoadScene(map.buildIndex);
+    }
+    public static void OnMapLoad(Scene map, LoadSceneMode mode) {
+        // Desativa o calculo de encontrar batalhas para "teleportar" os personagens
+        RECalculator.encountersEnabled = false;
+        // Pegar a informação da posição dos personagens pelo SO da party
+        // Reposicionar os game objects dos players na telas e recolocar referencias necessarias (Ex.: animator)
+        // To do
+        RECalculator.encountersEnabled = true;
+        SceneManager.sceneLoaded -= OnMapLoad;
+    }
+}
