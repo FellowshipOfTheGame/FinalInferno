d365b10e525d8132c81857289ac6a9cba9deaa43
Gabriel Salgueiro <gabrielsalgueiro@hotmail.com.br>
2019-05-21 19:56:40 -0300

    Data structure and unlock skills

--- Assets/Scripts/Scriptable Objects/Hero.cs
+++ Assets/Scripts/Scriptable Objects/Hero.cs
@@ -6,7 +6,14 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Hero", menuName = "ScriptableObject/Hero", order = 2)]
 public class Hero : Unit{
     public Sprite spriteOW; //"sprite" do heroi no "Over Wolrd"
-    public Sprite skillBG; //"sprite" de fundo da arvore de "skills" 
+    public Sprite skillBG; //"sprite" de fundo da arvore de "skills"  
+    public List<PlayerSkill> skillsToUpdate; //lista de skills que podem upar com o level do personagem
+    
+    public void UnlockSkills(){
+        foreach (PlayerSkill skill in skillsToUpdate){
+            skill.Unlock();
+        }
+    }
 
     public override Color DialogueColor { get { return color; } }
     public override string DialogueName { get { return (name == null)? "" : name; } }
--- Assets/Scripts/Scriptable Objects/PlayerSkill.cs
+++ Assets/Scripts/Scriptable Objects/PlayerSkill.cs
@@ -5,10 +5,38 @@ using UnityEngine;
 //engloba todas as "skills" dos personagens do jogador, que ganham nivel
 [CreateAssetMenu(fileName = "PlayerSkill", menuName = "ScriptableObject/PlayerSkill", order = 5)]
 public class PlayerSkill : Skill{
-   public int level; //inivel da "skill"
-   public long xp; //experiencia da "skill"
-   public long xpNext; //experiencia necessaria para a "skill" subir de nivel
-   public string description; //descricao da "skill" que aparecera para o jogador durante a batalha
-   public bool active; //sinaliza se a "skill" esta ativa ou nao
-    //(...) 
+    public int level; //inivel da "skill"
+    public long xp; //experiencia da "skill"
+    public long xpNext; //experiencia necessaria para a "skill" subir de nivel
+    public string description; //descricao da "skill" que aparecera para o jogador durante a batalha
+    public bool active; //sinaliza se a "skill" esta ativa ou nao
+    public List<PlayerSkill> skillsToUpdate;
+    public List<PlayerSkill> prerequisiteSkill;
+    public List<int> prerequisiteSkillLevel;
+    
+
+    public void UnlockSkills(){
+        foreach(PlayerSkill skill in skillsToUpdate){
+            skill.Unlock();
+        }
+    }
+
+    public void Unlock(){
+        bool check = true;
+
+        // foreach(PlayerSkill skill, int level in prerequisiteSkill, prerequisiteSkillLevel){
+        //     check &= checkskill.CheckLevel(level);
+        // }
+        for(int i = 0; i < prerequisiteSkill.Count; i++){
+            check &= prerequisiteSkill[i].CheckLevel(prerequisiteSkillLevel[i]);
+        }
+
+        if(check) this.level = 1;
+    }
+
+    public bool CheckLevel(int prerequisite){
+        return (level >= prerequisite);
+    }
+
+    
 }
