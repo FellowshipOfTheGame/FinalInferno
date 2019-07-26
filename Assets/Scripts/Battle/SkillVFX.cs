3d4407b8f0e3dd5fa0f524649f7ea3247c3019a2
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2019-07-26 10:26:26 -0300

    Adds Particle System for Skills
    Adds a particle system that can instantiate particles when skills are used (called by the skill's animation) and are deleted when the skill's animation ends. Adds the particle for Promise of Riches.Also adds the skill's art, animation and animator controller. Adds the art of instill fear, but its animation and controller are still missing.

--- Assets/Scripts/Battle/SkillVFX.cs
+++ Assets/Scripts/Battle/SkillVFX.cs
@@ -9,6 +9,7 @@ namespace FinalInferno{
     {
         public static int nTargets;
         private static int counter = 0;
+        private static GameObject particle = null;
         void UseSkill(){
             Debug.Log("Chamou o use skill pela animação");
             BattleSkillManager.currentSkill.Use(BattleSkillManager.currentUser, transform.parent.GetComponent<BattleUnit>());
@@ -21,7 +22,16 @@ namespace FinalInferno{
                 nTargets = -1;
                 FinalInferno.UI.FSM.AnimationEnded.EndAnimation();
             }
+
+            if(particle != null)
+                Destroy(particle);
+
             Destroy(gameObject);
+        }
+
+        void CreateParticles(GameObject particles)
+        {
+            particle = Instantiate(particles, new Vector3(transform.position.x, transform.position.y+((GetComponent<SpriteRenderer>()).size.y/2.0f), transform.position.z), transform.rotation);
         }
     }
 }
