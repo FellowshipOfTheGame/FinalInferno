0133b1f2b5337d55aff9c9837e1e9f0aeaf6bd9f
Unknown <giulianolourencon@gmail.com>
2019-08-08 16:12:56 -0300

    Starting party animation

---
+++ Assets/Scripts/UI/Battle/VictoryScreenAnimation.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static FinalInferno.BattleProgress;
+using UnityEngine.UI;
+
+namespace FinalInferno.UI.Victory
+{
+    public class VictoryScreenAnimation : MonoBehaviour
+    {
+        [Header("Party Level")]
+        [SerializeField] private Slider partyLevelSlider;
+        [SerializeField] private Image currentXPImage;
+        [SerializeField] private Text startingLevelText;
+        [SerializeField] private Text nextLevelText;
+
+        private BattleChanges changes;
+        [SerializeField] private float minDuration = 1f, maxDuration = 5f;
+
+        public void SetPartyStatus()
+        {
+            partyLevelSlider.value = BattleProgress.startingExp / (BattleProgress.startingExp + BattleProgress.xpToNextLevel);
+            currentXPImage.fillAmount = BattleProgress.startingExp / (BattleProgress.startingExp + BattleProgress.xpToNextLevel);
+            startingLevelText.text = startingLevel.ToString();
+            nextLevelText.text = (startingLevel + 1).ToString();
+
+        }
+
+        public void StartAnimation() 
+        {
+            changes = new BattleChanges(Party.Instance);
+            StartCoroutine(PartyLevel());
+        }
+
+        private IEnumerator PartyLevel()
+        {
+            
+            yield return null;
+        }
+    }
+}
