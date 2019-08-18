1cfe342072cb3f679e535471c04de19bf40979b2
Gabriel Salgueiro <gabrielsalgueiro@hotmail.com.br>
2019-08-17 23:29:48 -0300

    Skill Multilate added

---
+++ Assets/Scripts/Scriptable Objects/SkillEffects/LevelUpSkills.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "LevelUpSkills", menuName = "ScriptableObject/SkillEffect/LevelUpSkills")]
+    public class LevelUpSkills : SkillEffect {
+        // value1 = dmg multiplier
+        public override string Description1{ get{ return "x Damage"; } }
+        public override string Description2{ get{ return null; } }
+        public override void Apply(BattleUnit source, BattleUnit target) {
+            List<BattleUnit> team = BattleManager.instance.GetTeam(UnitType.Enemy);
+            foreach (EnemySkill skill in team[0].unit.skills){
+                skill.LevelUp();
+            }
+        }
+    }
+}
