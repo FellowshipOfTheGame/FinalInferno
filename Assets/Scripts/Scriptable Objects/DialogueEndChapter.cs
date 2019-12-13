bcfe67dfc7a6209090d1552115759ecf4280f1ce
Ítalo <italo.ts4895@gmail.com>
2019-12-13 10:43:39 -0300

    Fixed some tilemap and visual details
    The Third now vanished upon quest completion; Adjusted the position of
    some of the invisible barriers; Adjusted the position of some of the
    scene change triggers; Tries to spawn cerberus for the final battle
    insted of the placeholder Pwner;

--- Assets/Scripts/Scriptable Objects/DialogueEndChapter.cs
+++ Assets/Scripts/Scriptable Objects/DialogueEndChapter.cs
@@ -5,8 +5,6 @@ namespace FinalInferno{
     [CreateAssetMenu(fileName = "EndChapterDialogue", menuName = "ScriptableObject/DialogueSystem/DialogueEndChapter")]
     public class DialogueEndChapter : Fog.Dialogue.Dialogue
     {
-        [SerializeField] private FinalInferno.UI.FSM.ButtonClickDecision decision;
-        
         public override void AfterDialogue(){
             base.AfterDialogue();
             Fog.Dialogue.DialogueHandler.instance.StartCoroutine(WaitToGoBack());
