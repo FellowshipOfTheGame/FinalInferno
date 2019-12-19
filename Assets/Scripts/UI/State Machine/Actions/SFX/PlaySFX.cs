eeba28f13967f66b1abfc3754f52361732323b4f
Psy <giulianolourencon@gmail.com>
2019-12-19 14:18:30 -0300

    Added remaining UI SFX

---
+++ Assets/Scripts/UI/State Machine/Actions/SFX/PlaySFX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FinalInferno;
+using FinalInferno.UI.Battle;
+using FinalInferno.UI.AII;
+
+namespace FinalInferno.UI.FSM
+{
+    /// <summary>
+    /// Ação que toca uma musica na jukebox de BGM.
+    /// </summary>
+    [CreateAssetMenu(menuName = "BattleUI SM/Actions/Play Sound Effect")]
+    public class PlaySFX : ComponentRequester
+    {
+    public AudioClip clip;
+    private AudioSource source;
+
+        public override void Act(StateController controller)
+        {
+            source.clip = clip;
+            source.Play();
+        }
+
+        public override void RequestComponent(GameObject provider)
+        {
+            source = provider.GetComponent<AudioSource>();
+        }
+
+    }
+
+}
