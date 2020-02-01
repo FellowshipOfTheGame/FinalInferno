484b216279e8a762668069ca0bcb14ae809562d8
Ítalo <italo.ts4895@gmail.com>
2020-01-31 21:50:05 -0300

    Reverted skill xp per level change, but adjusted values so it doesnt level too fast

--- Assets/Scripts/Scriptable Objects/PlayerSkill.cs
+++ Assets/Scripts/Scriptable Objects/PlayerSkill.cs
@@ -81,7 +81,7 @@ namespace FinalInferno{
             while(xp >= xpNext && level < Table.Rows.Count-1){
                 level++;
                 // TO DO: Tabela externa com os valores de xpnext para cada level
-                xpNext = (level < 10)? 30 : (level < 25)?  60 : 100;
+                xpNext = level * 400;
                 
                 up = true;
             }
