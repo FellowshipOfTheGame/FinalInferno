c5127f625960860ad0f4ef703c3fae1de81e1c47
Ítalo <italo.ts4895@gmail.com>
2020-02-03 11:57:54 -0300

    Amidi buffs now affect resistance, corrected values for Herman passives
    Added ResistanceDown status effect; Added DecreaseResistance skill
    effect; PromisseOfRiches and InstillFear now afect resistance in the
    same way they affect defense; Added DecreaseResistance skill effect
    icon; Adjusted values for tables of Socialism and Surplus, they were
    using values for the removed IncreaseBuff and DecreaseDebuff skill
    effects instead of ModifyStatusEffect;

---
+++ Assets/Scripts/Battle/StatusEffects/ResistanceDown.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    public class ResistanceDown : StatusEffect {
+        public override StatusType Type { get{ return StatusType.Debuff; } }
+        public override float Value { get{ return resValue; } }
+        private int resValue;
+        private float valueReceived;
+
+        public ResistanceDown(BattleUnit src, BattleUnit trgt, float value, int dur = 1, bool force = false) {
+            if(dur < 0)
+                dur = int.MinValue;
+            Duration = dur;
+            TurnsLeft = Duration;
+            Target = trgt;
+            Source = src;
+            valueReceived = value;
+            resValue = Mathf.Max(Mathf.FloorToInt(trgt.curMagicDef * value), 1);
+            Failed = !Apply(force);
+        }
+
+        public override void CopyTo(BattleUnit target, float modifier = 1.0f){
+            target.AddEffect(new ResistanceDown(Source, target, valueReceived * modifier, Duration), true);
+        }
+
+        public override void Amplify(float modifier){
+            Target.curMagicDef += resValue;
+            resValue = Mathf.Max(Mathf.FloorToInt(modifier * resValue), 1);
+            Apply(true);
+        }
+
+        public override bool Apply(bool force = false) {
+            if(!base.Apply(force))
+                return false;
+            Target.curMagicDef -= resValue;
+            return true;
+        }
+
+        public override void Remove() {
+            Target.curMagicDef += resValue;
+            base.Remove();
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/SkillEffects/DecreaseResistance.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "DecreaseResistance", menuName = "ScriptableObject/SkillEffect/DecreaseResistance")]
+    public class DecreaseResistance : SkillEffect {
+        // value1 = magicDefUp multiplier
+        // value2 = buff duration
+        public override string Description { get { return "Decrease magical resistance by " + value1 * 100 + "% for " + value2 + " turns"; } }
+
+        public override void Apply(BattleUnit source, BattleUnit target) {
+            if(value2 < 0)
+                target.curMagicDef -= (int)value1 * target.curMagicDef;
+            else
+                target.AddEffect(new ResistanceDown(source, target, value1, (int)value2));
+        }
+    }
+}
