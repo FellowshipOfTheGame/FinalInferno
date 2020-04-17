1cf8993f7b5a0feff285d4395997c5d1d1f23803
Ítalo <italo.ts4895@gmail.com>
2020-04-17 00:16:06 -0300

    De Tox Juice Update
    Added LosingHPOverTime status effect, target loses a percentage of max
    hp each turn;
    Added DamageOverTimePercentage skill effect, applies LosingHPOverTime
    status effect with neutral element;
    De Tox Juice now uses DamageOverTimePercentage instead of
    DamageOverTime, table was already configured to use values compatible
    with it;

---
+++ Assets/Scripts/Scriptable Objects/SkillEffects/DamageOverTimePercentage.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "DamageOverTimePercentage", menuName = "ScriptableObject/SkillEffect/DamageOverTimePercentage")]
+    public class DamageOverTimePercentage : SkillEffect {
+        // value1 = MaxHP multiplier
+        // value2 = DoT duration
+        public override string Description { get {return "Deals " + value1 + "x of Target's Max HP as Neutral damage for " + value2 + " turns"; } }
+
+        public override void Apply(BattleUnit source, BattleUnit target) {
+            target.AddEffect(new LosingHPOverTime(source, target, value1, Element.Neutral, (int)value2));
+        }
+    }
+}
