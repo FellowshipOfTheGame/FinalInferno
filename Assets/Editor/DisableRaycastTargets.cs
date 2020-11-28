2e91f560abe9af33a6e0c1792f540954584bb7af
Ítalo <italo.ts4895@gmail.com>
2020-11-28 19:46:33 -0300

    Ui related game optimizations
    Pause Menu was being way too costly for the CPU, greatly reduced that cost;
    Added new Main Menu assets;
    Disabled UI raycasting in all game scenes (I hope);

---
+++ Assets/Editor/DisableRaycastTargets.cs
@@ -0,0 +1,27 @@
+﻿using UnityEditor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using UnityEngine.UI;
+
+public static class DisableRaycastTargets
+{
+    [MenuItem("Tools/Final Inferno/Disable Raycast Targets")]
+    static void DisableAllRaycastTargets(){
+        bool madeChanges = false;
+        foreach(GraphicRaycaster raycaster in GameObject.FindObjectsOfType<GraphicRaycaster>()){
+            if(raycaster.enabled) Debug.Log($"disabling raycaster {raycaster}");
+            madeChanges = raycaster.enabled || madeChanges;
+            raycaster.enabled = false;
+        }
+        foreach(Graphic graphicElement in GameObject.FindObjectsOfType<Graphic>()){
+            if(graphicElement.raycastTarget) Debug.Log($"disabling raycast target {graphicElement}");
+            madeChanges = graphicElement.raycastTarget || madeChanges;
+            graphicElement.raycastTarget = false;
+        }
+        if(madeChanges){
+            EditorSceneManager.MarkAllScenesDirty();
+        }
+    }
+}
--- Assets/Scripts/UI/Menus/SkillsContent.cs
+++ Assets/Scripts/UI/Menus/SkillsContent.cs
@@ -10,17 +10,36 @@ namespace FinalInferno.UI.SkillsMenu
         [SerializeField] private RectTransform content;
 
         private float xPosition = 0f;
+        private int curIndex = 0;
         private const float skillListWidth = 360f;
         private const float skillListsSpacing = 0f;
+        private bool isInPosition = false;
+
+        void Awake(){
+            isInPosition = false;
+            curIndex = 0;
+        }
 
         void Update()
         {
-            content.localPosition = new Vector3(Mathf.Lerp(content.localPosition.x, xPosition, .15f), content.localPosition.y);
+            if(!isInPosition && Mathf.Abs(content.localPosition.x - xPosition) >=  (skillListWidth * 0.01f)){
+                isInPosition = false;
+                content.localPosition = new Vector3(Mathf.Lerp(content.localPosition.x, xPosition, .25f), content.localPosition.y);
+            }else{
+                SetContentToPosition(curIndex, true);
+            }
         }
 
-        public void SetContentToPosition(int index)
+        public void SetContentToPosition(int index, bool skipAnimation = false)
         {
-            xPosition = -index * skillListWidth -(index * skillListsSpacing);
+            curIndex = index;
+            if(!skipAnimation){
+                isInPosition = false;
+                xPosition = -index * skillListWidth -(index * skillListsSpacing);
+            }else if(!isInPosition){
+                isInPosition = true;
+                content.localPosition = new Vector3(xPosition, content.localPosition.y);
+            }
         }
     }
 
