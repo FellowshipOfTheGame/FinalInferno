029aac186c5fd2bcc67c9fb91d6170afefb3a5d1
Ítalo <italo.ts4895@gmail.com>
2021-01-07 01:37:14 -0300

    Created UnitIndex to allow static references to units by index

---
+++ Assets/Scripts/Scriptable Objects/UnitIndex.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "UnitIndex", menuName = "ScriptableObject/UnitIndex")]
+    public class UnitIndex : ScriptableObject
+    {
+        [SerializeField] private List<Unit> unitList = new List<Unit>();
+        public ReadOnlyCollection<Unit> UnitList { get; private set; }
+
+        void OnEnable(){
+            UnitList = unitList.AsReadOnly();
+        }
+    }
+}
