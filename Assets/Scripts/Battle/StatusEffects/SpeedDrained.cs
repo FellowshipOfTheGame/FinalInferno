c5936efbedda9dd0a37b201f556f839fcf354b6b
Ítalo <italo.ts4895@gmail.com>
2021-02-12 19:14:23 -0300

    Created SpeedDrain skill effect and corresponding status effects

---
+++ Assets/Scripts/Battle/StatusEffects/SpeedDrained.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    public class SpeedDrained : StatusEffect {
+        public override StatusType Type { get{ return StatusType.Debuff; } }
+        public override float Value { get{ return spdValue; } }
+        private int spdValue;
+        private float valueReceived;
+
+        public SpeedDrained(BattleUnit src, BattleUnit trgt, float value, int dur = 1, bool force = false) {
+            if(dur < 0)
+                dur = int.MinValue;
+            Duration = dur;
+            TurnsLeft = Duration;
+            Target = trgt;
+            Source = src;
+            valueReceived = value;
+            spdValue = Mathf.Max(Mathf.FloorToInt(trgt.curSpeed * value), 1);
+            Failed = !Apply(force);
+        }
+
+        public override void CopyTo(BattleUnit target, float modifier = 1.0f){
+            target.AddEffect(new SpeedDrained(Source, target, valueReceived * modifier, Duration), true);
+        }
+
+        public override void Amplify(float modifier){
+            Target.curSpeed += spdValue;
+            spdValue = Mathf.Max(Mathf.FloorToInt(modifier * spdValue), 1);
+            Apply(true);
+        }
+
+        public override bool Apply(bool force = false) {
+            if(!base.Apply(force))
+                return false;
+            Target.curSpeed -= spdValue;
+            return true;
+        }
+
+        public override void Remove() {
+            Target.curSpeed += spdValue;
+            base.Remove();
+        }
+    }
+}
---
+++ Assets/Scripts/Scriptable Objects/SkillEffects/SpeedDrain.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "SpeedDrain", menuName = "ScriptableObject/SkillEffect/SpeedDrain")]
+    public class SpeedDrain : SkillEffect {
+        // value1 = speedDrain multiplier
+        // value2 = debuff duration
+        public override string Description { get { return "Drain " + value1*100 + "% speed for " + value2 + " turns"; } }
+        
+        public override void Apply(BattleUnit source, BattleUnit target) {
+            bool isDraining = false;
+            foreach(StatusEffect effect in target.effects){
+                if(effect.GetType() == typeof(SpeedDrained) && effect.Source == source){
+                    isDraining = true;
+                    break;
+                }
+            }
+            if(!isDraining){
+                source.AddEffect(new DrainingSpeed(source, target, value1, (int)value2));
+                target.AddEffect(new SpeedDrained(source, target, value1, (int)value2));
+            }
+        }
+    }
+}
