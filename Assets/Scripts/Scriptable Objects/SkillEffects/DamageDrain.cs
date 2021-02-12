c7512df7be6616839e4218fb5fadf97017b06c5d
Ítalo <italo.ts4895@gmail.com>
2021-02-12 18:40:02 -0300

    Created DamageDrain skill effect

---
+++ Assets/Scripts/Scriptable Objects/SkillEffects/DamageDrain.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "DamageDrain", menuName = "ScriptableObject/SkillEffect/DamageDrain")]
+    public class DamageDrain : SkillEffect {
+        // value1 = dmgDrain multiplier
+        // value2 = debuff duration
+        public override string Description { get { return "Drain " + value1*100 + "% damage for " + value2 + " turns"; } }
+        
+        public override void Apply(BattleUnit source, BattleUnit target) {
+            bool isDraining = false;
+            foreach(StatusEffect effect in target.effects){
+                if(effect.GetType() == typeof(DamageDrained) && effect.Source == source){
+                    isDraining = true;
+                    break;
+                }
+            }
+            if(!isDraining){
+                source.AddEffect(new DrainingDamage(source, target, value1, (int)value2));
+                target.AddEffect(new DamageDrained(source, target, value1, (int)value2));
+            }
+        }
+    }
+}
