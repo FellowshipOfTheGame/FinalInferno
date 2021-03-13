ca12986fecaa59f5e0a9c9c7415d4e2fc87316b5
Ítalo <italo.ts4895@gmail.com>
2021-03-13 17:11:02 -0300

    New eunuch satyr AI

---
+++ Assets/Scripts/Scriptable Objects/EnemiesAI/EunuchSatyr.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FinalInferno{
+    [CreateAssetMenu(fileName = "Eunuch Satyr", menuName = "ScriptableObject/Enemy/EunuchSatyr")]
+    public class EunuchSatyr : Enemy
+    {
+        private bool IsDrainingSpeed(BattleUnit unit){
+            if(unit == null || !(unit.unit is EunuchSatyr)) return false;
+            foreach(StatusEffect effect in unit.effects){
+                if(effect is DrainingSpeed && effect.Source == unit){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public override Skill AttackDecision(){
+            BattleUnit thisUnit = BattleManager.instance.currentUnit;
+            return (IsDrainingSpeed(thisUnit))? attackSkill : skills[0];
+        }
+    }
+}
