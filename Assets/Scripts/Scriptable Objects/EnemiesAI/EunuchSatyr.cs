239bd38d3e34052204167f5aa1e05df9a9654cbf
Ítalo <italo.ts4895@gmail.com>
2021-10-25 12:16:02 -0300

    Fix eunuch satyr missing preview

--- Assets/Scripts/Scriptable Objects/EnemiesAI/EunuchSatyr.cs
+++ Assets/Scripts/Scriptable Objects/EnemiesAI/EunuchSatyr.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace FinalInferno{
     [CreateAssetMenu(fileName = "Eunuch Satyr", menuName = "ScriptableObject/Enemy/EunuchSatyr")]
@@ -21,4 +24,16 @@ namespace FinalInferno{
             return (IsDrainingSpeed(thisUnit))? attackSkill : skills[0];
         }
     }
+
+    #if UNITY_EDITOR
+    [CustomPreview(typeof(EunuchSatyr))]
+    public class EunuchSatyrPreview : UnitPreview{
+        public override bool HasPreviewGUI(){
+            return base.HasPreviewGUI();
+        }
+        public override void OnInteractivePreviewGUI(Rect r, GUIStyle background){
+            base.OnInteractivePreviewGUI(r, background);
+        }
+    }
+    #endif
 }
