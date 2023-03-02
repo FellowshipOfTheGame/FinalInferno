4ca8d93b938dcad0e538270fa305b54a4e5dd99d
LeonardoTPereira <leonardo.t.pereira13@gmail.com>
2023-03-02 00:11:45 -0300

    Fixes tables with float, adds imp AI with table's chance to use skill
    Fixes enemies and skills tables where the floating point numbers were saved with comma instead of period.
    Adds Malformed Imp AI.
    Adds AI able to use the value inside the enemy's table as the chance to use their active skill.
    Adds table for Vengeful Ghost data (was missing).
    Fixes Golden Tormentor and Eunuch Satyr which were using the Wanderer's table.
    Fixes all tables to have probabilities to use each skill.

---
+++ Assets/Scripts/Scriptable Objects/EnemiesAI/EnemySingleActiveSkillUser.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace FinalInferno {
+
+    [CreateAssetMenu(fileName = "EnemySingleActiveSkill", menuName = "ScriptableObject/Enemy/SingleActiveSkill")]
+    public class EnemySingleActiveSkillUser : Enemy {
+        private EnemySkill ActiveSkill => (EnemySkill)skills[0];
+
+        public override Skill AttackDecision() {
+            Debug.LogWarning($"Probability: {ActiveSkill.Probability}");
+            float roll = Random.Range(0.0f, 1.0f);
+            return roll < ActiveSkill.Probability ? ActiveSkill : attackSkill;
+        }
+    }
+}
--- Assets/Scripts/Scriptable Objects/Skill.cs
+++ Assets/Scripts/Scriptable Objects/Skill.cs
@@ -2,15 +2,18 @@
 using UnityEngine;
 
 namespace FinalInferno {
+
     //engloba todas as "skills"
     [CreateAssetMenu(fileName = "Skill", menuName = "ScriptableObject/Skill")]
     public class Skill : ScriptableObject, IDatabaseItem {
         public const int maxCost = 20;
         public const int baseCost = 8;
+
         [Header("Skill")]
         public new string name;
+
         [SerializeField] protected int level;
-        public virtual int Level { get => level; set { } }
+        public virtual int Level { get => level; set => level = value; }
         public float cost;
         public bool active = true;
         [TextArea, SerializeField] protected string shortDescription;
@@ -18,6 +21,7 @@ namespace FinalInferno {
         public TargetType target;
         public Element attribute;
         [SerializeField] private SkillType type;
+
         public string TypeString {
             get => type switch {
                 SkillType.Active => "Active Skill",
@@ -32,18 +36,25 @@ namespace FinalInferno {
                 _ => ""
             };
         }
+
         public SkillType Type => type;
         [SerializeField] private int callbackDelay = 0;
         public List<SkillEffectTuple> effects;
+
         [Space(15)]
         [SerializeField] private GameObject visualEffect;
+
         public GameObject VisualEffect => visualEffect;
 
         #region IDatabaseItem
-        public virtual void LoadTables() { }
 
-        public virtual void Preload() { }
-        #endregion
+        public virtual void LoadTables() {
+        }
+
+        public virtual void Preload() {
+        }
+
+        #endregion IDatabaseItem
 
         // TO DO: Would work better as a function of TargetType when they become SOs
         public List<BattleUnit> FilterTargets(BattleUnit source, List<BattleUnit> oldList) {
@@ -57,35 +68,41 @@ namespace FinalInferno {
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.SingleDeadAlly:
                     case TargetType.AllDeadAllies:
                         if (!allies.Contains(unit) || unit.CurHP > 0)
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.SingleLiveAlly:
                     case TargetType.AllLiveAllies:
                         if (!allies.Contains(unit) || unit.CurHP <= 0)
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.AllAlliesLiveOrDead:
                         if (!allies.Contains(unit))
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.SingleDeadEnemy:
                     case TargetType.AllDeadEnemies:
                         if (allies.Contains(unit) || unit.CurHP > 0)
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.SingleLiveEnemy:
                     case TargetType.AllLiveEnemies:
                         if (allies.Contains(unit) || unit.CurHP <= 0)
                             newList.Remove(unit);
 
                         break;
+
                     case TargetType.AllEnemiesLiveOrDead:
                         if (allies.Contains(unit))
                             newList.Remove(unit);
@@ -136,6 +153,8 @@ namespace FinalInferno {
             ApplyAllSkillEffects(user, target, shouldOverride1, value1, shouldOverride2, value2);
         }
 
-        public virtual void ResetSkill() { Debug.LogError("Wrong use of skill reset", this); }
+        public virtual void ResetSkill() {
+            Debug.LogError("Wrong use of skill reset", this);
+        }
     }
 }
